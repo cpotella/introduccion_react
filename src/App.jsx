e9bf765c72961de6cc535ec513d7ce745bab4576
cpotella <cpotella@gmail.com>
2024-10-13 10:04:48 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Header from "./components/Header";
 import RegisterPage from "./components/Registro";
 import LoginPage from "./components/Login";
 import Cart from "./components/Cart";
@@ -10,9 +11,10 @@ function App() {
   return (
     <>
       <Navbar />
+      <Header />
       {/* <RegisterPage />
-      <LoginPage /> */}
-      {/* <Home /> */}
+      <LoginPage />
+      <Home /> */}
       <Cart/>
       <Footer />
     </>
